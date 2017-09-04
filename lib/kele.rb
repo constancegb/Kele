2ef1d54ec293589573b507afa271facd92797e05
Constance Gobert <constancegobert@hotmail.fr>
2017-09-04 12:40:44 +0200

    user story 3 - retrieve users (unfinished)

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -7,6 +8,11 @@ class Kele
   def initialize(email, password)
     response = self.class.post("/sessions", body: { "email": email, "password": password} )
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
   end
 
 end
